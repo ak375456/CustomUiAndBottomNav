d309c2fef911633279e51ffe9777258918da814f
ak375456 <ak375456@gmail.com>
2025-03-05 21:58:35 +0500

    Started and added button

---
+++ app/src/main/java/com/example/animationincompose/presentation/bottom_navigation/BottomNavigation.kt
@@ -0,0 +1,10 @@
+package com.example.animationincompose.presentation.bottom_navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+
+data class BottomNavigation(
+    val icon: ImageVector,
+    val label: String,
+    val iconDescription: String,
+)
---
+++ app/src/main/java/com/example/animationincompose/presentation/bottom_navigation/BottomNavigationScreensholder.kt
@@ -0,0 +1,9 @@
+package com.example.animationincompose.presentation.bottom_navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BottomNavigationScreenHolder(modifier: Modifier = Modifier) {
+    
+}
